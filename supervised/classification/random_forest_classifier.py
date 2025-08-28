2cb6d1a56907e4f77d4268b246429ca2325115b2
Mohd Nazeeb Mansoori <warsinazeeb@gmail.com>
2025-08-28 19:37:37 +0530

    Started implementation of Random Forest Regression

--- supervised/classification/random_forest_classifier.py
+++ supervised/classification/random_forest_classifier.py
@@ -3,12 +3,12 @@ from sklearn.datasets import load_iris
 import pandas as pd
 import numpy as np
 
-#Data loading and cleaning
+# Loading and preparing data
 df=load_iris()
 dataset=pd.DataFrame(df.data )
 dataset.columns=df.feature_names
 
-#Seperation of Dependent and Independent variable
+# Separating features and targete
 x=dataset
 y=df.target
 
@@ -16,10 +16,11 @@ y=df.target
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.30,random_state=42)
 
-#Importing model
+#Random Forest Classifier
 from sklearn.ensemble import RandomForestClassifier
 classiifier=RandomForestClassifier()
-#Hyper parameter tuning
+
+#Hyper parameter tuning using - GridSearchCV
 from sklearn.model_selection import GridSearchCV
 parameters = {
     'n_estimators': [100, 300, 500, 1000],
@@ -31,11 +32,12 @@ parameters = {
 random_forest_cv=GridSearchCV(classiifier,param_grid=parameters,scoring='accuracy',cv=5)
 random_forest_cv.fit(X_train,y_train)
 
-#Best parameters and estimators
+#Best parameters and scores
 print(random_forest_cv.best_params_)
 print(random_forest_cv.best_score_)
+print(random_forest_cv.best_estimator_)
 
-#Prediction and scoring
+#Prediction and Evaluation
 y_pred=random_forest_cv.predict(X_test)
 from sklearn.metrics import accuracy_score,classification_report
 print(accuracy_score(y_test,y_pred))
